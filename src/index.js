97faf50177f12da6f3e473e59dfc7fd1ed42dad5
StasPotapov <55351332+StasPotapov@users.noreply.github.com>
2019-12-21 18:37:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import articleConstructor from './articleConstructor';
+import api from './api';
+
+import './index.css';
+
+const root = document.getElementById('root');
+
+let url = 'https://en.wikipedia.org/w/api.php?origin=*';
+
+let articlesInfo = [];
+
+api().then(json => {
+  console.log(json.query.pages);
+  Object.values(json.query.pages).forEach(props => {
+    articlesInfo.push(articleConstructor(props));
+  });
+
+  let ul = document.createElement('ul');
+  articlesInfo.forEach(articleInfo => {
+    ul.append(articleInfo);
+  });
+  root.append(ul);
+});
